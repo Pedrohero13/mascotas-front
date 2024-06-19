ba0629400a53eccc51c25526b892ac14b852a3ae
Pedro Hernandez <pedrohernandeztecnomotum.com>
2024-06-19 11:23:17 -0600

    cofnigurar mascotas.service

--- src/app/core/services/mascotas.service.ts
+++ src/app/core/services/mascotas.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Mascotas } from '../models/mascotas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MascotasService {
-
+  private readonly http = inject(HttpClient)
   constructor() { }
+
+  obtenerMascotas(): Observable<Mascotas[]> {
+    return this.http.get<Mascotas[]>(`${environment.apiUrl}/api/v1/obtenerMascotas`);
+  }
+  obtenerMascota(id_mascota: number): Observable<Mascotas> {
+    return this.http.get<Mascotas>(`${environment.apiUrl}/api/v1/obtenerMascota/${id_mascota}`);
+  }
+  agregarMasctoa(mascota: Mascotas): Observable<Mascotas> {
+    return this.http.post<Mascotas>(`${environment.apiUrl}/api/v1/agregarMascota`, mascota);
+  }
+  modificarMascota(id_mascota: number,mascota: Mascotas): Observable<Mascotas> {
+    return this.http.put<Mascotas>(`${environment.apiUrl}/api/v1/modificarMascota/${id_mascota}`,mascota);
+  }
+
+  eliminarMascota(id_mascota: number): Observable<any> {
+    return this.http.delete<any>(`${environment.apiUrl}/api/v1/borrarMascota/${id_mascota}`);
+  }
 }
